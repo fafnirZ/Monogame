4514ec96825a8732f6f7034908ac601564d37d23
Jacky Xie <gokurohono@gmail.com>
2021-10-24 23:13:42 +1100

    initial commit

---
+++ Monogame/src/character/Character.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Monogame.src.character
+{
+    class Character
+    {
+        public Character()
+        {
+
+        }
+
+        // move function
+
+        // jump function
+
+
+
+    }
+}
